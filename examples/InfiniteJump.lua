d14aabf7f7884926cb0e16c51251dd9873b471d7
Laamy <83557555+Laamy@users.noreply.github.com>
2022-12-31 14:22:20 +1100

    Update InfiniteJump.lua

--- examples/InfiniteJump.lua
+++ examples/InfiniteJump.lua
@@ -1,9 +1,9 @@
 while true do
-	wait(0.05)
-	
-	if LocalPlayer.exists() then -- in a world
-		if Game.canUseKeys() then
-			LocalPlayer.setOnGround(true) -- airjump
-		end
-	end
+    wait(0.05)
+
+    -- If the local player exists and can use keys
+    if LocalPlayer.exists() and Game.canUseKeys() then
+        -- Set the player's on ground state to true
+        LocalPlayer.setOnGround(true)
+    end
 end
