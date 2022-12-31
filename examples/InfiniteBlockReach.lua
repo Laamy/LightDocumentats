132e3d2b2a061dcfdb918296243e258c2111cbd6
Laamy <83557555+Laamy@users.noreply.github.com>
2022-12-31 14:20:35 +1100

    Update InfiniteBlockReach.lua

--- examples/InfiniteBlockReach.lua
+++ examples/InfiniteBlockReach.lua
@@ -1,8 +1,12 @@
 while true do
-	wait(0.05)
-	
-	if LocalPlayer.exists() then -- in a world
-		GameMode.setBlockReach(256) -- blockreach
-    break
-	end
+    wait(0.05)
+
+    -- If the local player exists
+    if LocalPlayer.exists() then
+        -- Set the block reach to 256
+        GameMode.setBlockReach(256)
+
+        -- Exit the loop
+        break
+    end
 end
